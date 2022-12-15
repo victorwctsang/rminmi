bd4b0d5f0af972ed47b4394dd5129159cc1e691f
Victor Tsang <victorwctsang@gmail.com>
2022-12-15 15:55:41 +1100

    Added basic test for delta model case

--- tests/testthat/test-minmi.R
+++ tests/testthat/test-minmi.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Delta model point estimate works", {
+  theta.true <- 10000
+  K <- 18000
+  n <- 30
+  B <- 100
+  q <- 0.5
+
+  set.seed(0)
+  ages <- runif(n, theta.true, K)
+  u <- runif(B, 0 , 1)
+
+  EXPECTED_DELTA_MODEL_POINT_ESTIMATE <- K - q^(-1/n)*(K-min(ages))
+
+  expect_equal(estimate_quantile.minmi(K=K, W=ages, u=u, eps.mean=0, eps.sigma=0, q=q), EXPECTED_DELTA_MODEL_POINT_ESTIMATE)
 })
